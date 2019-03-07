a79ed1ed66b6fcab2d562e7e0bddff7a919634c1
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-03-07 13:06:04 -0600

    Initializing commit

---
+++ BCCA-Employer-Postings/src/main/java/com/codyvanderpoel/BCCAEmployerPostings/BccaEmployerPostingsApplication.java
@@ -0,0 +1,13 @@
+package com.codyvanderpoel.BCCAEmployerPostings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BccaEmployerPostingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BccaEmployerPostingsApplication.class, args);
+	}
+
+}
