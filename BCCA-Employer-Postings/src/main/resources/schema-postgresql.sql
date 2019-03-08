8fcfecf36fbc6bff29517b5e3a4680f305112f08
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-03-08 11:25:38 -0600

    Renamed a field and added a new table
    I renamed the id field in the jobs and then set up the new comments table with what I think it should have.

--- BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
+++ BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS jobs (
-    id SERIAL PRIMARY KEY NOT NULL,
+    jobId SERIAL PRIMARY KEY NOT NULL,
     company TEXT NOT NULL,
     position TEXT NOT NULL,
     location TEXT NOT NULL,
     benefits TEXT NOT NULL
 );
 
---ALTER TABLE jobs OWNER TO "user";
--- GRANT ALL ON TABLE jobs TO "user";
---
--- GRANT ALL PRIVILEGES ON DATABASE "application-db" TO "user";
---
--- INSERT INTO applications (company, position, location) VALUES ("Cspire", "Software Engineer", "Ridgeland, MS", "Available Benefits: Full Vision, Full Dental, Hull Health Care");
+CREATE TABLE IF NOT EXISTS comments (
+    id SERIAL PRIMARY KEY NOT NULL,
+    jobId INT FOREIGN KEY REFERENCES jobs(jobId) NOT NULL,
+    comment TEXT NOT NULL
+);
+-- INSERT INTO jobs (company, position, location) VALUES ("Cspire", "Software Engineer", "Ridgeland, MS", "Available Benefits: Full Vision, Full Dental, Hull Health Care");
+-- INSERT INTO comments (comment) VALUES ("A great opportunity!");
