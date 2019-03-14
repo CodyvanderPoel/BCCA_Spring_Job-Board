b2af9e42e1ad333569b5ece41d296754ae2a5d10
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-03-14 09:23:18 -0500

    Imported new Models and PostMappings
    I imported the Spring Models and PostMappings, and began to work on my Post Method.

--- BCCA-Employer-Postings/src/main/java/com/codyvanderpoel/BCCAEmployerPostings/Controllers/MakeJobPostingController.java
+++ BCCA-Employer-Postings/src/main/java/com/codyvanderpoel/BCCAEmployerPostings/Controllers/MakeJobPostingController.java
@@ -1,12 +1,20 @@
 package com.codyvanderpoel.BCCAEmployerPostings.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MakeJobPostingController {
     @GetMapping("/make_posting")
     public String index() {
         return "make_post";
+    }
+
+    @PostMapping("/make_posting")
+    public String index(Model model) {
+
+        return "redirect:/";
     }
 }
