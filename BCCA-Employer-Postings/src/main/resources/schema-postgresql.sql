04fecccd33479799411028a31659c45e9d88ae24
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-03-28 08:56:41 -0500

    Removed NOT NULL and replaced it with ON DELETE CASCADE. Added GRANT statements, and a SELECT to test if my POST method actually wrote to the database.

--- BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
+++ BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
@@ -9,8 +9,15 @@ CREATE TABLE IF NOT EXISTS jobs (
 
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY NOT NULL,
-    jobId INT REFERENCES jobs(jobId) NOT NULL,
+    jobId INT REFERENCES jobs(jobId) ON DELETE CASCADE,
     comment TEXT NOT NULL
 );
+GRANT ALL ON TABLE jobs TO "postings";
+GRANT ALL ON TABLE comments TO "postings";
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO  "postings";
+GRANT ALL PRIVILEGES ON DATABASE "postings" TO "postings";
+
 -- INSERT INTO jobs (company, position, location) VALUES ("Cspire", "Software Engineer", "Ridgeland, MS", "2+ years of experience", "Available Benefits: Full Vision, Full Dental, Hull Health Care");
 -- INSERT INTO comments (comment) VALUES ("A great opportunity!");
+
+SELECT * FROM jobs;
