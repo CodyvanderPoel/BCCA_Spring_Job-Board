bc2d1439e7467de392eedeed34885c97bd724654
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-03-29 09:28:48 -0500

    Added date_posted field to the jobs table, and added the title and body field to the comments table.

--- BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
+++ BCCA-Employer-Postings/src/main/resources/schema-postgresql.sql
@@ -4,20 +4,22 @@ CREATE TABLE IF NOT EXISTS jobs (
     position TEXT NOT NULL,
     location TEXT NOT NULL,
     prereqs TEXT NOT NULL,
-    benefits TEXT NOT NULL
+    benefits TEXT NOT NULL,
+    date_posted TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY NOT NULL,
-    jobId INT REFERENCES jobs(jobId) ON DELETE CASCADE,
-    comment TEXT NOT NULL
+    jobId INT REFERENCES jobs(jobId) ON DELETE CASCADE NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL
 );
 GRANT ALL ON TABLE jobs TO "postings";
 GRANT ALL ON TABLE comments TO "postings";
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO  "postings";
 GRANT ALL PRIVILEGES ON DATABASE "postings" TO "postings";
 
--- INSERT INTO jobs (company, position, location) VALUES ("Cspire", "Software Engineer", "Ridgeland, MS", "2+ years of experience", "Available Benefits: Full Vision, Full Dental, Hull Health Care");
+--INSERT INTO jobs (company, position, location, prereqs, benefits) VALUES ('Cspire', 'Software Engineer', 'Ridgeland,MS', '2+ years of experience', 'Available Benefits: Full Vision, Full Dental, Hull Health Care');
 -- INSERT INTO comments (comment) VALUES ("A great opportunity!");
 
 SELECT * FROM jobs;
