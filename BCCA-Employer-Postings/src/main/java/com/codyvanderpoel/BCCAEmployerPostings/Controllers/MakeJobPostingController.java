89316e98777101d7bd94798062be0b827edca38d
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-03-29 09:30:18 -0500

    Added controllers that control the employer port of the site

--- BCCA-Employer-Postings/src/main/java/com/codyvanderpoel/BCCAEmployerPostings/Controllers/MakeJobPostingController.java
+++ BCCA-Employer-Postings/src/main/java/com/codyvanderpoel/BCCAEmployerPostings/Controllers/MakeJobPostingController.java
@@ -5,8 +5,13 @@ import com.codyvanderpoel.BCCAEmployerPostings.Models.JobPostingForm;
 import com.codyvanderpoel.BCCAEmployerPostings.Repositories.PostgresJobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MakeJobPostingController {
@@ -19,8 +24,21 @@ public class MakeJobPostingController {
     }
 
     @GetMapping("/employer_home")
-    public String getEmployerHome(){
+    public String getEmployerHome(Model model){
+        List<JobPosting> jobs = jobRepository.findAll();
+        model.addAttribute("jobs", jobs);
         return "employer_home";
+    }
+
+    @GetMapping("/employer_view_jobs/{jobId}")
+    public String getJobDetails(Model model, @PathVariable(value="jobId") String jobId){
+        var job = jobRepository.findById(Integer.parseInt(jobId));
+        if ((job).isPresent()){
+            model.addAttribute("job", job.get());
+            return "view_employer_post";
+        }else{
+            return "404";
+        }
     }
 
     @GetMapping("/employer_home/make_posting")
